1758741beb58b798423903b4b4d3853deb815d9e
GiorgioWolterGai <118194394+GiorgioWolterGai@users.noreply.github.com>
2022-11-22 17:34:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-   int primo_numero=0;
-    int secondo_numero=0;
+   int primo_numero = 0;
+    int secondo_numero = 0;
     int n;
     cin >> n;
-    int i=2;
-    if (n>= 2){
+    int i = 2;
+    if (n >= 2) {
         cout << 1 << endl;
-        while (i++ < n){
+        while (i++ <= n) {
             cout << primo_numero + secondo_numero << endl;
             if (primo_numero < secondo_numero)
                 primo_numero += secondo_numero;
@@ -17,6 +17,6 @@ int main() {
         }
         }else {
         cout << "errore" << endl;
-        }
+    }
    return 0;
 }
